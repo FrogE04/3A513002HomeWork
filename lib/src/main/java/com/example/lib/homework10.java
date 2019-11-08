ee8d4e0ab6dc0c2c37f0052e1cdcfe7669d78db4
FrogE04 <57343879+FrogE04@users.noreply.github.com>
2019-11-08 12:09:22 +0800

    Update homework10.java

--- lib/src/main/java/com/example/lib/homework10.java
+++ lib/src/main/java/com/example/lib/homework10.java
@@ -9,15 +9,21 @@ import java.util.Scanner;
             change(c);
         }
 
-        private static void change(char c) {
-            if(c>='A' && c<='Z'){
+        private static void change(char c)
+        {
+            if(c>='A' && c<='Z')
+            {
                 c+=32;
                 System.out.println("input"+(char)(c-32)+"change:"+c);
-            }else if(c>='a' && c<='z'){  //如果輸入的是小寫，-32即可得大小寫
+            }
+            else if(c>='a' && c<='z')
+            {  
                 c-=32;
                 System.out.println("input"+(char)(c+32)+"change:"+c);
-            }else{
-                System.out.println("error！！");
+            }
+            else
+            {
+                System.out.println("Error");
             }
         }
 }
