ac0fabfe4cf766f95c25f4cd07065ea25bf63914
FrogE04 <57343879+FrogE04@users.noreply.github.com>
2019-11-08 12:11:31 +0800

    Update homework12.java

--- lib/src/main/java/com/example/lib/homework12.java
+++ lib/src/main/java/com/example/lib/homework12.java
@@ -10,18 +10,23 @@ public class homework12 {
         int i, j, k, m;
         n = sc.nextInt();
         m = n;
-        if (n <= 100) {
-            for (k = 1; k <= m; k++) {
-                for (j = n; j < m; j++) {
+        if (n <= 100) 
+        {
+            for (k = 1; k <= m; k++) 
+            {
+                for (j = n; j < m; j++) 
+                {
                     System.out.print(" ");
                 }
-                for (i = 1; i <= n; i++) {
+                
+                for (i = 1; i <= n; i++)
+                {
                     System.out.print("*");
                 }
+                
                 System.out.print("\n");
                 n--;
-            }
-
-        }
-    }
+             }
+          }
+     }
 }
